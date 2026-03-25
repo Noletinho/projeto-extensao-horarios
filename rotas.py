573c5c0e87c39cf2c015a1dbf8c2c9b901c09a49
Noletinho <nolletoj@gmail.com>
2026-03-25 00:13:01 -0300

    Adiciona cadastro, listagem e exclusao de professores

--- rotas.py
+++ rotas.py
@@ -1,10 +1,59 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+def conectar():
+    conexao = sqlite3.connect('escola_horarios.db')
+    conexao.row_factory = sqlite3.Row
+    return conexao
+
 @app.route('/')
 def index():
-    return 'ola mundo'
+    return render_template('index.html')
+
+
+@app.route('/professores')
+def listar_professores():
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute('SELECT * FROM professor')
+    professores = cursor.fetchall()
+    conexao.close()
+    return render_template('professores.html', professores=professores)
+
+@app.route('/cadastro_professor')
+def cadastro_professor():
+    return render_template('cadastro_professor.html')
+
+@app.route('/salvar-professor', methods=['POST'])
+def salvar_professor():
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    email = request.form['email']
+    telefone = request.form['telefone']
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute("""
+    INSERT INTO professor (nome, cpf, email, telefone)
+    VALUES (?, ?, ?, ?)
+""", (nome, cpf, email, telefone))
+    conexao.commit()
+    conexao.close()
+
+    return 'Professor salvo com sucesso!'
+
+@app.route('/deletar_professor/<int:id_professor>', methods=['POST'])
+def deletar_professor(id_professor):
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute('DELETE FROM professor WHERE id_professor = ?', (id_professor,))
+    conexao.commit()
+    conexao.close()
+
+    return redirect(url_for('listar_professores'))
 
 if __name__ == "__main__":
     app.run(debug=True)
